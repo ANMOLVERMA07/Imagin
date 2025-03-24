f254151de6864887663dd2d96398623470d6f733
Anmol Verma <153612130+ANMOLVERMA07@users.noreply.github.com>
2025-03-24 15:59:40 +0000

    debug

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { createContext,useEffect,useState } from "react";
+import { createContext,useState } from "react";
 import { toast } from 'react-toastify';
 
 export const AppContext = createContext();
